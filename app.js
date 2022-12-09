a2b308307b3e536e28022a2b66b97916e1d0617c
tzigri <tzihgri@gmail.com>
2022-12-09 17:54:17 +0200

    Initial Commit

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const http = require('http');
-const hostname = 'localhost';
+const fs = require('fs');
 const port = 4000;
 
-const server = http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello for Node JS');
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 4001 });
+
+wss.on('connection', function connection(ws) {
+  console.log('Client connected');
+  const interval = setInterval(() => {
+    ws.send((new Date).toLocaleTimeString());
+  }, 1000)
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+  ws.onerror = function () {
+    console.log("Some Error occurred");
+  }
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
-})
+http.createServer(function (req, res) {
+  fs.readFile("./www/index.html", function (err, data) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data);
+    res.end();
+  });
+}).listen(port);
+console.log("Running at port " + port);
+
 
